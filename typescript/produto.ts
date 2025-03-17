1e03cea00ad21803e4107ff1d5e2e21daf4992d8
Julio Francisco Bernardino <juliobernardinoprogramador@gmail.com>
2025-03-17 18:33:46 -0300

    criação das classes pedido e produto

---
+++ typescript/produto.ts
@@ -0,0 +1,9 @@
+class Produto{
+    constructor (
+        public idProduto: number,
+        public nome: string,
+        public descricao: string,
+        public preco: number,
+        public idCategoria: number
+    ) {}
+}
