75f1785abf53deb00caf3075a40c156d19f9cd5f
Julio Francisco Bernardino <juliobernardinoprogramador@gmail.com>
2025-03-18 17:15:20 -0300

    update

--- js/login.js
+++ js/login.js
@@ -29,3 +29,15 @@ function login() {
         alert(error.message);
     });
 }
+
+const container = document.querySelector('.container');
+const registerBtn = document.querySelector('.register-btn');
+const loginBtn = document.querySelector('.login-btn');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add('active');
+})
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove('active');
+})
