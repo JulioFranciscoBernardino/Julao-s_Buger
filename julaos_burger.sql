ea35346512d012ad64d5ff9ae803fe7b01db906c
Julio Francisco Bernardino <juliofranciscobernardino@gmail.com>
2025-07-11 13:57:52 -0300

    Update julaos_burger.sql

--- julaos_burger.sql
+++ julaos_burger.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 19, 2025 at 06:22 AM
+-- Generation Time: Jul 11, 2025 at 06:57 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,73 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categoria`
---
-
-CREATE TABLE `categoria` (
-  `idcategoria` int(11) NOT NULL,
-  `nome` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `endereco`
---
-
-CREATE TABLE `endereco` (
-  `idendereco` int(11) NOT NULL,
-  `cpf` char(11) NOT NULL,
-  `endereco` varchar(255) NOT NULL,
-  `cidade` varchar(100) NOT NULL,
-  `estado` varchar(50) NOT NULL,
-  `cep` char(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `funcionario`
---
-
-CREATE TABLE `funcionario` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `senha` varchar(255) NOT NULL,
-  `tipo` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pedido`
---
-
-CREATE TABLE `pedido` (
-  `idpedido` int(11) NOT NULL,
-  `cpf` char(11) NOT NULL,
-  `data_pedido` timestamp NOT NULL DEFAULT current_timestamp(),
-  `status` varchar(20) NOT NULL,
-  `idendereco` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pedidoproduto`
---
-
-CREATE TABLE `pedidoproduto` (
-  `idpedidoproduto` int(11) NOT NULL,
-  `idpedido` int(11) NOT NULL,
-  `idproduto` int(11) NOT NULL,
-  `quantidade` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `produto`
 --
 
@@ -100,137 +33,24 @@ CREATE TABLE `produto` (
   `descricao` text NOT NULL,
   `preco` decimal(10,2) NOT NULL,
   `imagem` varchar(500) DEFAULT NULL,
-  `idcategoria` int(11) NOT NULL
+  `idcategoria` int(11) NOT NULL,
+  `ativo` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `saborbebida`
---
-
-CREATE TABLE `saborbebida` (
-  `idsaborbebida` int(11) NOT NULL,
-  `idproduto` int(11) NOT NULL,
-  `sabor` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `usuario`
---
-
-CREATE TABLE `usuario` (
-  `cpf` char(11) NOT NULL,
-  `nome` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `senha` varchar(255) NOT NULL,
-  `tipo` varchar(10) NOT NULL,
-  `pontos` int(11) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `usuario`
---
-
-INSERT INTO `usuario` (`cpf`, `nome`, `email`, `senha`, `tipo`, `pontos`) VALUES
-('48290020856', 'Julio', 'francisco@gmail.com', '$argon2id$v=19$m=65536,t=3,p=4$LcxWnNZcIVRYP0kifjMiQA$XtQPMwLPKdH3fB004FA3zu0usR2tPiSbXbACdzGQ/50', 'cliente', 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `categoria`
---
-ALTER TABLE `categoria`
-  ADD PRIMARY KEY (`idcategoria`);
-
---
--- Indexes for table `endereco`
---
-ALTER TABLE `endereco`
-  ADD PRIMARY KEY (`idendereco`),
-  ADD KEY `cpf` (`cpf`);
-
---
--- Indexes for table `funcionario`
---
-ALTER TABLE `funcionario`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- Indexes for table `pedido`
---
-ALTER TABLE `pedido`
-  ADD PRIMARY KEY (`idpedido`),
-  ADD KEY `cpf` (`cpf`),
-  ADD KEY `idendereco` (`idendereco`);
-
---
--- Indexes for table `pedidoproduto`
---
-ALTER TABLE `pedidoproduto`
-  ADD PRIMARY KEY (`idpedidoproduto`),
-  ADD KEY `idpedido` (`idpedido`),
-  ADD KEY `idproduto` (`idproduto`);
-
---
 -- Indexes for table `produto`
 --
 ALTER TABLE `produto`
   ADD PRIMARY KEY (`idproduto`),
-  ADD KEY `idcategoria` (`idcategoria`);
-
---
--- Indexes for table `saborbebida`
---
-ALTER TABLE `saborbebida`
-  ADD PRIMARY KEY (`idsaborbebida`),
-  ADD KEY `idproduto` (`idproduto`);
-
---
--- Indexes for table `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`cpf`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD KEY `fk_produto_categoria` (`idcategoria`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `categoria`
---
-ALTER TABLE `categoria`
-  MODIFY `idcategoria` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `endereco`
---
-ALTER TABLE `endereco`
-  MODIFY `idendereco` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `funcionario`
---
-ALTER TABLE `funcionario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pedido`
---
-ALTER TABLE `pedido`
-  MODIFY `idpedido` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pedidoproduto`
---
-ALTER TABLE `pedidoproduto`
-  MODIFY `idpedidoproduto` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `produto`
@@ -239,46 +59,14 @@ ALTER TABLE `produto`
   MODIFY `idproduto` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `saborbebida`
---
-ALTER TABLE `saborbebida`
-  MODIFY `idsaborbebida` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `endereco`
---
-ALTER TABLE `endereco`
-  ADD CONSTRAINT `endereco_ibfk_1` FOREIGN KEY (`cpf`) REFERENCES `usuario` (`cpf`) ON DELETE CASCADE;
-
---
--- Constraints for table `pedido`
---
-ALTER TABLE `pedido`
-  ADD CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`cpf`) REFERENCES `usuario` (`cpf`) ON DELETE CASCADE,
-  ADD CONSTRAINT `pedido_ibfk_2` FOREIGN KEY (`idendereco`) REFERENCES `endereco` (`idendereco`) ON DELETE CASCADE;
-
---
--- Constraints for table `pedidoproduto`
---
-ALTER TABLE `pedidoproduto`
-  ADD CONSTRAINT `pedidoproduto_ibfk_1` FOREIGN KEY (`idpedido`) REFERENCES `pedido` (`idpedido`) ON DELETE CASCADE,
-  ADD CONSTRAINT `pedidoproduto_ibfk_2` FOREIGN KEY (`idproduto`) REFERENCES `produto` (`idproduto`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `produto`
 --
 ALTER TABLE `produto`
-  ADD CONSTRAINT `produto_ibfk_1` FOREIGN KEY (`idcategoria`) REFERENCES `categoria` (`idcategoria`) ON DELETE CASCADE;
-
---
--- Constraints for table `saborbebida`
---
-ALTER TABLE `saborbebida`
-  ADD CONSTRAINT `saborbebida_ibfk_1` FOREIGN KEY (`idproduto`) REFERENCES `produto` (`idproduto`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_produto_categoria` FOREIGN KEY (`idcategoria`) REFERENCES `categoria` (`idcategoria`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
